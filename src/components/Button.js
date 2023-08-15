2bc4fb393fb503bbc8cb46fea88c8869c0555354
dvcorreia1984 <victor@onsleer.co.za>
2023-08-15 18:40:37 +0200

    Updated Calculator.js and Buttons.js file

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 // eslint-disable-next-line react/prop-types
-const Button = ({ value, htmlId, onClick }) => (
-  <button id={htmlId} type="button" onClick={onClick}>
+const Button = ({ htmlId, value, onClick }) => (
+  <button id={htmlId} type="button" onClick={() => onClick(value)}>
     {value}
   </button>
 );
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -17,40 +17,41 @@ const Calculator = () => {
 
   const displayValue = calculatorData.next || calculatorData.total || '0';
 
+  const buttons = [
+    { htmlId: 'white', value: 'AC' },
+    { htmlId: 'white', value: '+/-' },
+    { htmlId: 'white', value: '%' },
+    { htmlId: 'orange', value: '÷' },
+    { htmlId: 'white', value: '7' },
+    { htmlId: 'white', value: '8' },
+    { htmlId: 'white', value: '9' },
+    { htmlId: 'orange', value: 'x' },
+    { htmlId: 'white', value: '4' },
+    { htmlId: 'white', value: '5' },
+    { htmlId: 'white', value: '6' },
+    { htmlId: 'orange', value: '-' },
+    { htmlId: 'white', value: '1' },
+    { htmlId: 'white', value: '2' },
+    { htmlId: 'white', value: '3' },
+    { htmlId: 'orange', value: '+' },
+    { htmlId: 'zero', value: '0' },
+    { htmlId: 'white', value: '.' },
+    { htmlId: 'orange', value: '=' },
+  ];
+
   return (
     <div id="wrapper">
       <div id="display">
         <p>{displayValue}</p>
       </div>
-      <div id="row">
-        <Button htmlId="white" value="AC" onClick={() => handleButtonClick('AC')} />
-        <Button htmlId="white" value="+/-" onClick={() => handleButtonClick('+/-')} />
-        <Button htmlId="white" value="%" onClick={() => handleButtonClick('%')} />
-        <Button htmlId="orange" value="÷" onClick={() => handleButtonClick('÷')} />
-      </div>
-      <div id="row">
-        <Button htmlId="white" value="7" onClick={() => handleButtonClick('7')} />
-        <Button htmlId="white" value="8" onClick={() => handleButtonClick('8')} />
-        <Button htmlId="white" value="9" onClick={() => handleButtonClick('9')} />
-        <Button htmlId="orange" value="x" onClick={() => handleButtonClick('x')} />
-      </div>
-      <div id="row">
-        <Button htmlId="white" value="4" onClick={() => handleButtonClick('4')} />
-        <Button htmlId="white" value="5" onClick={() => handleButtonClick('5')} />
-        <Button htmlId="white" value="6" onClick={() => handleButtonClick('6')} />
-        <Button htmlId="orange" value="-" onClick={() => handleButtonClick('-')} />
-      </div>
-      <div id="row">
-        <Button htmlId="white" value="1" onClick={() => handleButtonClick('1')} />
-        <Button htmlId="white" value="2" onClick={() => handleButtonClick('2')} />
-        <Button htmlId="white" value="3" onClick={() => handleButtonClick('3')} />
-        <Button htmlId="orange" value="+" onClick={() => handleButtonClick('+')} />
-      </div>
-      <div id="row">
-        <Button htmlId="zero" value="0" onClick={() => handleButtonClick('0')} />
-        <Button htmlId="white" value="." onClick={() => handleButtonClick('.')} />
-        <Button htmlId="orange" value="=" onClick={() => handleButtonClick('=')} />
-      </div>
+      {buttons.map((button) => (
+        <Button
+          key={button.value}
+          htmlId={button.htmlId}
+          value={button.value}
+          onClick={handleButtonClick}
+        />
+      ))}
     </div>
   );
 };
