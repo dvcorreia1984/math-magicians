19eba9c5f500b124c0dc439aeee6ad8b4b7feeb6
dvcorreia1984 <victor@onsleer.co.za>
2023-08-15 10:51:54 +0200

    Updated Calculator.js file

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,53 +1,39 @@
 import React from 'react';
 import './Calculator.css';
 
-const Calculator = () => (
-  <div id="wrapper">
-    <div id="display">
-      <p>0</p>
-    </div>
-    <div id="row">
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-      <button id="orange" type="button">
-        ÷
-      </button>
-    </div>
-    <div id="row">
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button id="orange" type="button">
-        x
-      </button>
-    </div>
-    <div id="row">
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button id="orange" type="button">
-        -
-      </button>
-    </div>
-    <div id="row">
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
-      <button id="orange" type="button">
-        +
-      </button>
-    </div>
-    <div id="row">
-      <button id="zero" type="button">
-        0
-      </button>
-      <button type="button">.</button>
-      <button id="orange" type="button">
-        =
-      </button>
-    </div>
-  </div>
-);
+const Calculator = () => React.createElement('div', { id: 'wrapper' }, [
+  React.createElement('div', { id: 'display' }, [
+    React.createElement('p', {}, '0'),
+  ]),
+  React.createElement('div', { id: 'row' }, [
+    React.createElement('button', { type: 'button' }, 'AC'),
+    React.createElement('button', { type: 'button' }, '+/-'),
+    React.createElement('button', { type: 'button' }, '%'),
+    React.createElement('button', { id: 'orange', type: 'button' }, '÷'),
+  ]),
+  React.createElement('div', { id: 'row' }, [
+    React.createElement('button', { type: 'button' }, '7'),
+    React.createElement('button', { type: 'button' }, '8'),
+    React.createElement('button', { type: 'button' }, '9'),
+    React.createElement('button', { id: 'orange', type: 'button' }, 'x'),
+  ]),
+  React.createElement('div', { id: 'row' }, [
+    React.createElement('button', { type: 'button' }, '4'),
+    React.createElement('button', { type: 'button' }, '5'),
+    React.createElement('button', { type: 'button' }, '6'),
+    React.createElement('button', { id: 'orange', type: 'button' }, '-'),
+  ]),
+  React.createElement('div', { id: 'row' }, [
+    React.createElement('button', { type: 'button' }, '1'),
+    React.createElement('button', { type: 'button' }, '2'),
+    React.createElement('button', { type: 'button' }, '3'),
+    React.createElement('button', { id: 'orange', type: 'button' }, '+'),
+  ]),
+  React.createElement('div', { id: 'row' }, [
+    React.createElement('button', { id: 'zero', type: 'button' }, '0'),
+    React.createElement('button', { type: 'button' }, '.'),
+    React.createElement('button', { id: 'orange', type: 'button' }, '='),
+  ]),
+]);
 
 export default Calculator;
