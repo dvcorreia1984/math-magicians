a8a7c5a7d9c53cc5659d39081d4c5da0f55dccd4
dvcorreia1984 <victor@onsleer.co.za>
2023-08-22 15:04:53 +0200

    Added Navbar.module.css file and contents

--- src/routes/Navbar.jsx
+++ src/routes/Navbar.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => (
-  <div className="navBar">
-    <div className="home">
-      <Link to="/">Home</Link>
-    </div>
-    <div className="calculator">
-      <Link to="/calculator">Calculator</Link>
-    </div>
-    <div className="quote">
-      <Link to="/quote">Quote</Link>
+  <div className={styles.navBar}>
+    <h1 style={{ justifyContent: 'flex-start' }}>Math Magicians</h1>
+    <div className={styles.menu}>
+      <div className="home">
+        <Link to="/">Home</Link>
+      </div>
+      <div>|</div>
+      <div className="calculator">
+        <Link to="/calculator">Calculator</Link>
+      </div>
+      <div>|</div>
+      <div className="quote">
+        <Link to="/quote">Quote</Link>
+      </div>
     </div>
   </div>
 );
