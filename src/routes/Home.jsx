ec5ba0f7cfc798ca6f22c639045e2445ee0d50e0
dvcorreia1984 <victor@onsleer.co.za>
2023-08-22 11:35:49 +0200

    Added Pages for routing

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,3 +1,25 @@
-const Home = () => <h2>Home</h2>;
+const Home = () => (
+  <>
+    <h2>Welcome to our Page!</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+      assumenda sunt omnis molestias tempora ad modi fuga provident magni! Omnis
+      voluptas amet enim quo, incidunt sequi sapiente ducimus exercitationem
+      harum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+      assumenda sunt omnis molestias tempora ad modi fuga provident magni! Omnis
+      voluptas amet enim quo, incidunt sequi sapiente ducimus exercitationem
+      harum.
+    </p>
 
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+      assumenda sunt omnis molestias tempora ad modi fuga provident magni! Omnis
+      voluptas amet enim quo, incidunt sequi sapiente ducimus exercitationem
+      harum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+      assumenda sunt omnis molestias tempora ad modi fuga provident magni! Omnis
+      voluptas amet enim quo, incidunt sequi sapiente ducimus exercitationem
+      harum.
+    </p>
+  </>
+);
 export default Home;
