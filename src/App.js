b4056f2249f8931ac09bd31c282808863e818ede
dvcorreia1984 <victor@onsleer.co.za>
2023-08-22 16:48:30 +0200

    Enabled disabled linters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import QuotePage from './routes/QuotePage';
